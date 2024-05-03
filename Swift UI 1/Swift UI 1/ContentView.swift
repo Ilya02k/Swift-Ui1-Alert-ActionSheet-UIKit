be5fe030ae1a47c704fea10207061b9a89e8320d
Ilya Kazlou <ilya02k@gmail.com>
2024-05-03 19:01:54 +0300

    alert and action sheet

---
+++ Swift UI 1/Swift UI 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift UI 1
+//
+//  Created by Ann Dosova on 2.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift UI 1/Swift UI 1/Swift_UI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_1App.swift
+//  Swift UI 1
+//
+//  Created by Ann Dosova on 2.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
